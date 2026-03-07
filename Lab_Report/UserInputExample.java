dc32c10aac99d67bd22990d3b76c7529af31766f
Akhil Hamal <akhilhamal2005@gmail.com>
2026-03-08 02:47:32 +0545

    New lab Report programs

---
+++ Lab_Report/UserInputExample.java
@@ -0,0 +1,21 @@
+package Lab_Report;
+import java.util.Scanner;
+
+public class UserInputExample
+{
+    public static void main(String[] kgf)
+    {
+        Scanner input =new Scanner(System.in);
+        System.err.println("Enter your ID:");
+        int id=input.nextInt();
+        input.nextLine();
+        System.err.println("Enter your Name:");
+        String name=input.nextLine();
+        System.out.println("\nUser Details:");
+        System.out.println("ID: " + id);
+        System.out.println("Name: " + name);
+        input.close();
+    }
+    
+
+}
---
+++ Lab_Report/WelcomeJava.java
@@ -0,0 +1,9 @@
+package Lab_Report;
+
+public class WelcomeJava
+{
+    public static void main(String [] akjei)
+    {
+        System.err.println("Welcome to java programming");
+    }
+}
